a8b1a27109c749a9d1748f04c880e5d4e901d96a
jinook929 <jinook929@gmail.com>
2021-01-13 20:03:18 -0500

    commit tree 4eddeaad2610f044a543c3b70c9244e464852f49

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 def my_all?(collection)
+    block_return_value = []
+    if block_given?
+        i = 0
+        while i < collection.length
+            block_return_value.push(yield(collection[i]))
+            i += 1
+        end
+    end
 
+    if block_return_value.include?(false)
+        false
+    else
+        true
+    end
 end
+
+# p my_all?([1,2,3])
